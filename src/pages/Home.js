5fa27712af17ca9e6c45cb28dd53f25a73115538
MattCameron1991 <matthew.c.cameron91@gmail.com>
2020-10-22 19:43:38 +1100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,59 +4,59 @@ import Storage from "../utility/StorageUtil";
 import { sortAscending } from "../utility/stringUtil";
 
 export function Home(props) {
-    return (
-        <div>
-            <h1>Home</h1>
-            <div>
-                <button
-                    onClick={(e) => {
-                        e.preventDefault();
+	return (
+		<div>
+			<h1>Home</h1>
+			<div>
+				<button
+					onClick={(e) => {
+						e.preventDefault();
 
-                        let monsters = Object.values(Storage.monsterDict);
-                        monsters.sort((a, b) => sortAscending(a.Name, b.Name));
+						let monsters = Object.values(Storage.monsterDict);
+						monsters.sort((a, b) => sortAscending(a.Name, b.Name));
 
-                        let harvestedItems = Object.values(
-                            Storage.harvestableItemDict
-                        );
-                        harvestedItems.sort((a, b) =>
-                            sortAscending(a.ReferenceId, b.ReferenceId)
-                        );
+						let harvestedItems = Object.values(
+							Storage.harvestableItemDict
+						);
+						harvestedItems.sort((a, b) =>
+							sortAscending(a.ReferenceId, b.ReferenceId)
+						);
 
-                        let trinketTables = Object.values(
-                            Storage.trinketTableDict
-                        );
-                        trinketTables.sort((a, b) =>
-                            sortAscending(
-                                a.TrinketTableType,
-                                b.TrinketTableType
-                            )
-                        );
+						let trinketTables = Object.values(
+							Storage.trinketTableDict
+						);
+						trinketTables.sort((a, b) =>
+							sortAscending(
+								a.TrinketTableType,
+								b.TrinketTableType
+							)
+						);
 
-                        let craftableItems = Object.values(
-                            Storage.craftableItemDict
-                        );
-                        craftableItems.sort((a, b) =>
-                            sortAscending(a.Name, b.Name)
-                        );
+						let craftableItems = Object.values(
+							Storage.craftableItemDict
+						);
+						craftableItems.sort((a, b) =>
+							sortAscending(a.Name, b.Name)
+						);
 
-                        fileSaveAs(
-                            "system-state.json",
-                            JSON.stringify({
-                                Monsters: monsters,
-                                HarvestedItems: harvestedItems,
-                                TrinketTables: trinketTables,
-                                CraftableItems: craftableItems,
-                            })
-                        );
-                    }}
-                >
-                    Save State
-                </button>
-            </div>
-            <div>
-                <input type="text" style={{ width: "100%", height: "4em" }} />
-            </div>
-            <div>{JSON.stringify(props)}</div>
-        </div>
-    );
+						fileSaveAs(
+							"system-state.json",
+							JSON.stringify({
+								Monsters: monsters,
+								HarvestedItems: harvestedItems,
+								TrinketTables: trinketTables,
+								CraftableItems: craftableItems,
+							})
+						);
+					}}
+				>
+					Save State
+				</button>
+			</div>
+			<div>
+				<input type="text" style={{ width: "100%", height: "4em" }} />
+			</div>
+			{/* <div>{JSON.stringify(props)}</div> */}
+		</div>
+	);
 }
