f6c3e33d2701eea36b20a5b1f78fc08bafb3ecf0
MattCameron1991 <34317520+MattCameron1991@users.noreply.github.com>
2020-11-13 15:50:33 +1100

    Update MonsterDisplay.js

--- src/components/MonsterDisplay.js
+++ src/components/MonsterDisplay.js
@@ -262,7 +262,7 @@ function ACHPSpeed(props) {
   let hpOut = (
     <>
       {monster.health.average} (
-      {<DynamicRender toRender={`{@dice ${monster.health.formula}}`} />}
+      {<DynamicRender toRender={`{@dice ${monster.health.formula}}`} />})
     </>
   );
   topBlock.push(
